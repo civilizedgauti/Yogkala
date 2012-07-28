5a5f92ee270610155f5d0d12552792ecd92e00e2
Jinesh Patel <caprikorn_ji@yahoo.co.in>
2012-07-29 01:20:43 +0530

    Static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,31 +1,70 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'Yogkala'" do
+    
+    it "should have the h1 'Yogkala'" do
      visit '/static_pages/home'
-     page.should have_content('Yogkala')
+     page.should have_selector('h1', :text => 'Yogkala')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Yogkala")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
-  describe "About page" do
-    it "should have the content 'About'" do
-     visit '/static_pages/about'
-     page.should have_content('About')
+  it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Yogkala")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title', :text => '| About')
     end
   end
 
-  describe "Contact page" do
-    it "should have the content 'Contact'" do
-     visit '/static_pages/contact'
-     page.should have_content('Contact')
+  it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Yogkala")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/contact'
+      page.should_not have_selector('title', :text => '| Contact')
     end
   end
 
-  describe "Help page" do
-    it "should have the content 'Help'" do
-     visit '/static_pages/help'
-     page.should have_content('Help')
+ it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Yogkala")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => '| Help')
     end
   end
 end
