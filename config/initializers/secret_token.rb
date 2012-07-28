bed917e9d66a98c6f54ff355ea53f6cd435cd4d8
Jinesh Patel <caprikorn_ji@yahoo.co.in>
2012-07-28 16:20:12 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yogkala::Application.config.secret_token = '74f813d97db102a67e4f906039fccf56ef146042b32c126a61869522577d5c49f20de4a73ab7cf1d35f3ae77ca2fd7f1c290d76ac82b91a479bf02a010fae5ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yogkala::Application.config.session_store :cookie_store, key: '_Yogkala_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yogkala::Application.config.session_store :active_record_store
